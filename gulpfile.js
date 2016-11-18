7694366a8d0ab714cbe825b23a7e9ed40cad76a4
Cerega32 <Cerega32@mail.ru>
2016-11-19 02:36:32 +0300

    Полностью сделана главная страница

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,13 @@ var pug = require("gulp-pug");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
+var server = require("browser-sync");
+var mqpacker = require("css-mqpacker");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var svgstore = require("gulp-svgstore");
+var svgmin = require("gulp-svgmin");
 
 gulp.task("style", function() {
   gulp.src("less/style.less")
@@ -17,17 +23,53 @@ gulp.task("style", function() {
         "last 2 Firefox versions",
         "last 2 Opera versions",
         "last 2 Edge versions"
-      ]})
-    ]))
+      ]}),
+      mqpacker ({
+          sort: true
+      })
+  ]))
     .pipe(gulp.dest("css"))
-    .pipe(server.stream());
+    .pipe(server.stream())
+    .pipe(minify())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("css"));
 });
  
 gulp.task("pug", function buildHTML() {
   return gulp.src("pug/*.pug")
+      .pipe(plumber())
       .pipe(pug({pretty: true}))
       .pipe( gulp.dest("")); 
 });
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+      .pipe(imagemin([
+          imagemin.optipng({optimizationLevel: 3}),
+          imagemin.jpegtran({progressive: true})
+  ]))
+      .pipe(gulp.dest("img")); 
+});
+
+gulp.task("images", function() {
+  return gulp.src("img/**/*.{png,jpg,gif}")
+      .pipe(imagemin([
+          imagemin.optipng({optimizationLevel: 3}),
+          imagemin.jpegtran({progressive: true})
+  ]))
+      .pipe(gulp.dest("img")); 
+});
+
+gulp.task("symbols", function() {
+    return gulp.src("img/icons/*.svg")
+        .pipe(svgmin())
+        .pipe(svgstore({
+            inlineSvg: true
+        }))
+        .pipe(rename("symbols.svg"))
+        .pipe(gulp.dest("img"));
+});
+
 
 gulp.task("serve", ["style"], function() {
   server.init({
--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+$(document).ready(function(){
+  $('.rewiews__items').slick({
+    autoplay: true, 
+    dots: false,
+    pauseOnDotsHover: true,
+    responsive: [
+    {
+      breakpoint: 1200,
+      settings: {
+        arrows: false,
+        dots: true
+      }
+    }]
+  });
+});
+
+$(document).ready(function(){
+  $('.price__items').slick({
+    responsive:[
+        {
+            breakpoint: 10000,
+            settings: "unslick"
+        },
+        {
+          breakpoint: 700,
+          settings: {
+            centerMode: true,
+            centerPadding: '20px',  
+            arrows: false,
+            dots: true,
+            infinite: false,
+            initialSlide: 1
+          }
+    }]
+  });
+});
+
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
 
